fd4f9e04070b9ddfd5a227806fd5caf870602f95
shelly@019mobile.co.il <shelly@019mobile.co.il>
2025-01-13 10:51:34 +0100

    after creating XML structure

--- libraries/normalization.ts
+++ libraries/normalization.ts
@@ -7,3 +7,15 @@ function normalization_1(session : any, event : any, localParams: any ){
    session.normalizedNumber=x;
 return true;
 }
+
+
+function createXML(session : any, event : any, localParams: any ){
+   let x : string;
+   session.xml1= "<?xml version=\"1.0\" encoding=\"UTF-8\"?><sms><user><username>SendSMSwhileOff</username><password>Aa87!6179</password></user><source>019</source><destinations><phone>";
+
+   session.xml2= "</phone></destinations><message>test</message><response>0</response></sms>";
+
+   session.xml= session.xml1 + session.normalizedNumber + session.xml2;
+return true;
+}
+
