57ac8b3c049505481cfbe64166137e398de24a35
shelly@019mobile.co.il <shelly@019mobile.co.il>
2025-01-13 10:43:02 +0100

    final

--- libraries/normalization.ts
+++ libraries/normalization.ts
@@ -1,6 +1,9 @@
 
 function normalization_1(session : any, event : any, localParams: any ){
 
-   localParams.X= session.initial.SIP.From.address.uri.user  //972507000118
-return  "test:Demo_13_01_2025:test_demo_130125";
+   let x : string;
+   x= session.initial.SIP.From.address.uri.user  //972507000118
+   x.substring(3, 11);
+   session.normalizedNumber=x;
+return true;
 }
