4275730e273f46d5f545ccf981fc4b3d8853c557
markus.ziegler@hcltech.com <markus.ziegler@hcltech.com>
2025-01-13 11:27:56 +0100

    Update

--- libraries/normalization.ts
+++ libraries/normalization.ts
@@ -2,7 +2,7 @@
 function normalization_1(session : any, event : any, localParams: any ){
 
    let x : string;
-   x= event.initial.SIP.From.address.uri.user  //972507000118
+   x = event.initial.SIP.From.address.uri.user;  //972507000118
    x.substring(3, 11);
    session.normalizedNumber=x;
 return true;
