9ed5494bc8c01ebf18a12d1f4eb2cf0c2bfc8442
shelly@019mobile.co.il <shelly@019mobile.co.il>
2025-01-13 11:10:35 +0100

    fix x

--- libraries/normalization.ts
+++ libraries/normalization.ts
@@ -2,7 +2,7 @@
 function normalization_1(session : any, event : any, localParams: any ){
 
    let x : string;
-   x= session.initial.SIP.From.address.uri.user  //972507000118
+   x= event.initial.SIP.From.address.uri.user  //972507000118
    x.substring(3, 11);
    session.normalizedNumber=x;
 return true;
@@ -18,4 +18,3 @@ function createXML(session : any, event : any, localParams: any ){
    session.xml= session.xml1 + session.normalizedNumber + session.xml2;
 return true;
 }
-
