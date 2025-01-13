824dfc73d605321c862b55a2765fae291cfe99b4
markus.ziegler@hcltech.com <markus.ziegler@hcltech.com>
2025-01-13 15:24:07 +0100

    Update

--- libraries/normalization.ts
+++ libraries/normalization.ts
@@ -9,15 +9,13 @@ try {
    session.s_initialSIP_lib = event;
    fromURI = event.SIP.From.address.uri.user;  //972507000118
    log.debug("Log From-1: {}", fromURI);
-   session.normalizedNumber = fromURI.substring(3, 11);
-   //session.normalizedNumber = fromURI;
+   session.s_normalizedNumber = fromURI.substring(3, 11);
 
    return "success";
 
 } catch (e) {
 
    log.debug("Log: {}", e);
-   log.debug("Log From-2: {}", session.normalizedNumber);
    return "error";
 }
 
@@ -26,11 +24,14 @@ try {
 
 
 function createXML(session : any, event : any, localParams: any ){
+
    let x : string;
-   session.xml1= "<?xml version=\"1.0\" encoding=\"UTF-8\"?><sms><user><username>SendSMSwhileOff</username><password>Aa87!6179</password></user><source>019</source><destinations><phone>";
+   session.s_xml1= "<?xml version=\"1.0\" encoding=\"UTF-8\"?><sms><user><username>SendSMSwhileOff</username><password>Aa87!6179</password></user><source>019</source><destinations><phone>";
 
-   session.xml2= "</phone></destinations><message>test</message><response>0</response></sms>";
+   session.s_xml2= "</phone></destinations><message>test</message><response>0</response></sms>";
 
-   session.xml= session.xml1 + session.normalizedNumber + session.xml2;
-return true;
+   session.s_xml= session.xml1 + session.normalizedNumber + session.xml2;
+
+   return "success";
+
 }
