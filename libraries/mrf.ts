090bea17438944ca745a1eac7346296f5cdcb09d
admin@admin.com <admin@admin.com>
2025-01-16 10:53:00 +0000

    inputvalidation save in session mrf object

--- libraries/mrf.ts
+++ libraries/mrf.ts
@@ -4,21 +4,37 @@ function inputvalidation(session : any, event : any, localParams: any ){
     let log = session.log;
 
     try {
-        if ( event["earlydialog"] != null ) {
-            log.debug("earlydialog: {}", event["earlydialog"]);
-        } else {
-            return "error.input.earlydialog";
-        }
         //promptandcollect, playannouncement
         if ( (event["action"] != null) && ((event["action"]==="promptandcollect") || (event["action"]==="playannouncement")) ) {
             log.debug("action: {}", event["action"]);
+            if ( event["action"]==="promptandcollect" ) 
+                session["mrf"]["action"] = "promptandcollect";
+            else if ( event["action"]==="playannouncement" ) 
+                session["mrf"]["action"] = "playannouncement";
+            else
+                return "error.input.actionincorrect";            
         } else {
-            return "error.input.action";
+            return "error.input.actionmissing";
         }
-        return "success";
+
+        //early dialog is boolean true/false
+        if ( event["earlydialog"] != null ) {
+            log.debug("earlydialog: {}", event["earlydialog"]);
+            if ( event["earlydialog"] === true) {
+                session["mrf"]["earlydialog"] = true;
+                return "true";
+            } else if ( event["earlydialog"] === false) {
+                session["mrf"]["earlydialog"] = false;
+                return "false";
+            } else {
+                return "error.input.earlydialogincorrect";
+            }
+        } else {
+            return "error.input.earlydialog";
+        }
     } catch (e) {
         log.debug("Log: {}", e);
-        return "error";
+        return "error.exception";
     }
 }
 
