4ab34d2205cf038995fb1b364e1c12d70dc760d8
admin@admin.com <admin@admin.com>
2025-01-16 10:44:22 +0000

    initial inputvalidation

---
+++ libraries/mrf.ts
@@ -0,0 +1,25 @@
+
+
+function inputvalidation(session : any, event : any, localParams: any ){
+    let log = session.log;
+
+    try {
+        if ( event["earlydialog"] != null ) {
+            log.debug("earlydialog: {}", event["earlydialog"]);
+        } else {
+            return "error.input.earlydialog";
+        }
+        //promptandcollect, playannouncement
+        if ( (event["action"] != null) && ((event["action"]==="promptandcollect") || (event["action"]==="playannouncement")) ) {
+            log.debug("action: {}", event["action"]);
+        } else {
+            return "error.input.action";
+        }
+        return "success";
+    } catch (e) {
+        log.debug("Log: {}", e);
+        return "error";
+    }
+}
+
+
