c6dd4080de196ee49e79df55eb704141ad8a9172
robert.stoiber@hcltech.com <robert.stoiber@hcltech.com>
2025-01-17 11:58:06 +0100

    add armMRFevents

--- libraries/mrf.ts
+++ libraries/mrf.ts
@@ -95,6 +95,50 @@ function inputvalidation(session : any, event : any, localParams: any ){
     }
 }
 
+function armMRFevents(sessionData:any,eventData:any,localParams:any): any {
+ let ret: ResultCode ;
+
+    let status2 : string;
+    let events : Events;
+    events = events || {};
+    events.InfoPollEvent="null";
+    events.SuccessResponsePollEvent="null";
+    events.RawContentPollEvent="test/test";
+
+    let headerVars : HeaderVars;
+    headerVars = headerVars || {};
+    headerVars.disableSendDefaultReason = "Disabled";
+    headerVars.disableSendNoAnswerReason = "Disabled";
+
+    let ringingTones : [RingingTone];
+    ringingTones = ringingTones || [];
+    let comf : RingingTone;
+    comf = comf || {};
+    comf.anno_name="comfort";
+    comf.anno_type=Annotype.CONNECT;
+    let ring : RingingTone;
+    ring = ring || {};
+    ring.anno_name="ringing";
+    ring.anno_type=Annotype.RINGING;
+    ringingTones.push(comf, ring);
+
+    let capabilities = sessionData.inCapabilities;
+    if( capabilities!=null){
+        capabilities.push(Capabilities.PEM);
+        capabilities.push(Capabilities.FORKING);
+        sessionData.outCapabilities = JSON.stringify(capabilities);
+    }
+
+    sessionData.events = JSON.stringify(events);
+    sessionData.headerrulevar=JSON.stringify(headerVars);
+    sessionData.headerrulesselect = "SipServiceSpecificRulesSet";
+    sessionData.ringingtones = JSON.stringify(ringingTones);
+    sessionData.upstreamCapabilities=JSON.stringify([]);
+
+    return "success";
+}
+
+
 function handle200OKINVITE(session:any,event:OCCPSIP.OCCPEvent,localParams:any) {
     let log = session.log;
 
@@ -126,7 +170,7 @@ function handle200OKINVITE(session:any,event:OCCPSIP.OCCPEvent,localParams:any) 
         }
         
     } catch (e) {
-        log.debug("Log: {}", e);
+        log.debug("handle200OKINVITE Log: {}", e);
         return "error.exception";
     }
 }
