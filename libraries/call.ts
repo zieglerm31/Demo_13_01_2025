cd3945c3324594210d4f5419d7578ad5eaeea1e5
robert.stoiber@hcltech.com <robert.stoiber@hcltech.com>
2025-01-20 10:32:32 +0100

    initial version

---
+++ libraries/call.ts
@@ -0,0 +1,44 @@
+
+
+function armevents(sessionData:any,eventData:any,localParams:any): any {
+ let ret: ResultCode ;
+
+    let status2 : string;
+    let events : Events;
+    events = events || {};
+    events.InfoPollEvent="null";
+    events.SuccessResponsePollEvent="null";
+    events.RawContentPollEvent="test/test";
+
+    let headerVars : HeaderVars;
+    headerVars = headerVars || {};
+    headerVars.disableSendDefaultReason = "Disabled";
+    headerVars.disableSendNoAnswerReason = "Disabled";
+
+    let ringingTones : [RingingTone];
+    ringingTones = ringingTones || [];
+    let comf : RingingTone;
+    comf = comf || {};
+    comf.anno_name="comfort";
+    comf.anno_type=Annotype.CONNECT;
+    let ring : RingingTone;
+    ring = ring || {};
+    ring.anno_name="ringing";
+    ring.anno_type=Annotype.RINGING;
+    ringingTones.push(comf, ring);
+
+    let capabilities = sessionData.inCapabilities;
+    if( capabilities!=null){
+        capabilities.push(Capabilities.PEM);
+        capabilities.push(Capabilities.FORKING);
+        sessionData.outCapabilities = JSON.stringify(capabilities);
+    }
+
+    sessionData.events = JSON.stringify(events);
+    sessionData.headerrulevar=JSON.stringify(headerVars);
+    sessionData.headerrulesselect = "SipServiceSpecificRulesSet";
+    sessionData.ringingtones = JSON.stringify(ringingTones);
+    sessionData.upstreamCapabilities=JSON.stringify([]);
+
+    return "success";
+}
