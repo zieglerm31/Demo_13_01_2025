4beeee84327cd80e61be606f720edb9353d3a3eb
robert.stoiber@hcltech.com <robert.stoiber@hcltech.com>
2025-01-20 09:58:09 +0100

    initial version

---
+++ libraries/ocshttp.ts
@@ -0,0 +1,76 @@
+
+
+
+function createXML(session : any, event : any, localParams: any ){
+
+   let x : string;
+   session.s_xml1= "<?xml version=\"1.0\" encoding=\"UTF-8\"?><sms><user><username>SendSMSwhileOff</username><password>Aa87!6179</password></user><source>019</source><destinations><phone>";
+
+   session.s_xml2= "</phone></destinations><message>test</message><response>0</response></sms>";
+
+   session.s_xml= session.xml1 + session.normalizedNumber + session.xml2;
+
+   return "success";
+
+}
+
+function checkHttpResponse(session : any, event : any, localParams: any ){
+    let log : any = session.log;
+    log.debug("Event received:{}",event);
+    let bodyStr=event.body;
+    session.restResponse =0;
+    session.callstate  ="HTTPRESPONSE_INIT";
+    session.CalledPartyOverwritten="sip:+4090123123@test.com"
+    try {
+        //let body = JSON.parse(bodyStr);
+        let body = event.body;
+        session.restResponse = body.status;
+        if (session.restResponse=="0") {
+            session.callstate  ="HTTPRESPONSE_CONNECT";   
+            if (body.paramX.match(session.Callingipv4)) {
+                session.CalledPartyOverwritten="sip:+4090123123@test.com"
+                session.loginfo = session.loginfo+"ParamX.match.OverwriteDestination. connect to "+session.CalledPartyOverwritten+";"; 
+            }  else if (body.paramY.match(session.Callingipv4)) {
+                session.CalledPartyOverwritten="sip:+4090123123@test.com"
+                session.loginfo = session.loginfo+"ParamY.match.OverwriteDestination. connect to "+session.CalledPartyOverwritten+";"; 
+            }  else {                
+                session.CalledPartyOverwritten=session.CalledParty;
+                session.loginfo = session.loginfo+"Param.do not match.Connect to Initial CalledParty;"; 
+            }
+        } else if (session.restResponse=="4") {
+            session.callstate ="HTTPRESPONSE_MRFPC";
+        } else if (session.restResponse=="5") {
+            session.callstate ="HTTPRESPONSE_MRFBYE";
+        } else {
+            session.callstate ="HTTPRESPONSE_UNKNOWN"+session.callstate;
+        }
+    } catch (e) {
+        log.error("checkHttpResponse Exception {}",e);
+        session.callstate  ="HTTPRESPONSE_EXCEPTION";
+    };
+
+    
+    let callstateold :string = session.callstate;    
+    log.error("Call_{}:State_{}->{}+",session["trace-skey"],callstateold,session.callstate);   
+    session.loginfo = session.loginfo+session.callstate+";"; 
+
+    //session.CalledPartyOverwritten=["sip:+4090123123@test.com","sip:+4090123124@test.com"];
+
+    return "scenario."+session.restResponse;
+}
+
+
+function failureHttpResponse(session : any, event : any, localParams: any ){     
+    let eventname: string=   event["event-name"];
+    if (event["event-name"].startsWith("sip")) {
+        session.loginfo = session.loginfo+"SIP-event received while waiting for Http response ("+eventname+");"; 
+        //bypass the TERIMATE call handler as the call is already terminated
+        return "sip.callended"
+    } else if (event["event-name"].startsWith("http")) {
+        session.loginfo = session.loginfo+"HttpFailure response received ("+event.body.status+");"; 
+    } else {
+        session.loginfo = session.loginfo+"No SIP or HTTP event received while waiting for Http response ("+eventname+");"; 
+    }
+    //return event.body.status;
+    return "success";
+}
