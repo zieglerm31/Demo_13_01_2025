dc037d21f7af233e81afd58180118e1e6d58975d
robert.stoiber@hcltech.com <robert.stoiber@hcltech.com>
2025-01-20 11:14:10 +0100

    add callended

--- libraries/call.ts
+++ libraries/call.ts
@@ -42,3 +42,58 @@ function armevents(sessionData:any,eventData:any,localParams:any): any {
 
     return "success";
 }
+
+function callended(session : any, eventData : any, localParams: any ){
+    let ret: string = "success";
+    /* enter here your code */
+
+    let log : any = session.log;
+    let callstateold = session.callstate;
+    session.callstate  ="CLOSED";
+    session.timeend= Math.floor(new Date()/1000);
+    try {
+        if (session.timeanswer!=null) {
+            session.duration= session.timeend - session.timeanswer;
+        } else {
+            session.duration="not-started";    
+        }
+    } catch (e) {
+        session.duration="exception";
+    }
+
+    log.error("Call_{}:State_{}->{}+",session["trace-skey"],callstateold,session.callstate);  
+    session.loginfo = session.loginfo+session.callstate+";duration="+session.duration+";";   
+
+    //prepare the LOG line - send to the log_sipdemo process 
+    let logline : LOG = {};
+    try{
+        logline.MSISDN = session.CalledParty;
+        logline.ServiceSessionId = session["fsm-id"];
+        logline.MsgType = "SIP.INVITE";
+        logline.MsgSessionId = session["fsm-id"];
+        logline.MsgId = "callid";
+        logline.MsgDetails = session.loginfo;
+        logline.Servicedetails = "";
+        session.loginfo="";
+
+        //timestamp added by GW before it put it into the queue to RTE
+        logline.GwEventTime = session.timeanswer; 
+
+        let now : Date = new Date();
+        let logDate : string = formatDate(now);
+        let logTime : string = formatTime(now);
+        logline.LogLineTime = logDate+ "." + logTime + "."+("000" + now.getMilliseconds().toString()).slice(-3);        
+    } catch (e) {
+        logline.MSISDN = "";
+        logline.ServiceSessionId = "";
+        logline.MsgType = "";
+        logline.MsgSessionId = "";
+        logline.MsgId = "";
+        logline.MsgDetails = "";
+        logline.Servicedetails = "error at CCRContinue logline";
+        logline.LogLineTime = "";
+    };
+    logline.TraceLevel = session["fsm-trace-level"];    
+    session.logline=logline;    
+    return ret;
+}
