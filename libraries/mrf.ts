48404d3a4346b60ba21e82ed8e489f249a08689c
robert.stoiber@hcltech.com <robert.stoiber@hcltech.com>
2025-02-17 11:23:20 +0100

    add SendINFOPromptandCollect - remove group

--- libraries/mrf.ts
+++ libraries/mrf.ts
@@ -107,11 +107,13 @@ function SendINFOPromptandCollect(session : any, event : any, localParams: any )
     content = content + session["mrf"]["downStreamToTag"];
     content = content + "\" type=\"application/moml+xml\">\n";
     //content = content + "<play interval=\"100ms\" iterate=\"1\" cleardb=\"true\" maxtime=\"50000ms\" barge=\"true\">\n <audio uri=\"file:///appl/wav/simpleplay.wav\"/>\n <playexit>\n   <exit namelist=\"play.end play.amt\"/>\n </playexit>\n</play>\n</dialogstart>\n</msml>";
-    content = content + "<group topology=\"parallel\">\n";
+
+    //OCMP response - groups not supported     <?xml version="1.0" encoding="UTF-8" standalone="yes"?><msml version="1.1">    <result response="402">        <description>Groups not supported</description>    </result></msml>
+    //content = content + "<group topology=\"parallel\">\n";
     content = content + "<play id=\"beforebargeplay\"><audio uri=\"file:///appl/wav/simpleplay.wav\" format=\"audio/wav\"  /><playexit><send target=\"collect\" event=\"starttimer\"/></playexit></play>\n";
     content = content + "<collect cleardb=\"true\" fdt=\"5s\" idt=\"3s\"><pattern digits=\"x\"><send target=\"source\" event=\"dialognamedefault\" namelist=\"dtmf.digits dtmf.end\"/></pattern>\n";
     content = content + "<detect><send target=\"play.beforebargeplay\" event=\"terminate\"/></detect>\n<noinput><send target=\"source\" event=\"dialognamedefault\" namelist=\"dtmf.digits dtmf.end\"/></noinput>\n<nomatch><send target=\"source\" event=\"dialognamedefault\" namelist=\"dtmf.digits dtmf.end\"/></nomatch>\n</collect>\n";
-    content = content + "</group>\n";
+    //content = content + "</group>\n";
     content = content + "</dialogstart>\n</msml>\n";
 
     let outevent = {
