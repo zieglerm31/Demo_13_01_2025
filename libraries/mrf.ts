a69b399e4308a55bc449b6233c5b7f2fc8262229
robert.stoiber@hcltech.com <robert.stoiber@hcltech.com>
2025-02-19 14:11:23 +0100

    update SendINFOPromptandCollect for PandC and PA only - dynamic

--- libraries/mrf.ts
+++ libraries/mrf.ts
@@ -101,33 +101,112 @@ using collect
 
 function SendINFOPromptandCollect(session : any, event : any, localParams: any ){
     //set the xml conn id --> session["mrf"]["downStreamToTag"]
+    //this function builds the MRF XML and sends it. a lib is used as the handerl sends only a default one that is only using play.end play.amt wihtout dtmf.
 
-/*
-    let outevent = {
-    "callid": session["fsm-id"],
-    "event-type": "sip",
-    "queue": "TASV4_1",
-    "id": 2,
-    "timestamp": 1739784834563,
-    "action": {
-      "legaction": "performMediaOperation",
-      "performMediaOperation": {
-        "ContentType": "application/msml+xml",
-        "Content": "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<msml  version=\"1.1\">\n<dialogstart name=\"annoPromptCollect\" target=\"conn:5b007c40\" type=\"application/moml+xml\">\n<play interval=\"100ms\" iterate=\"1\" cleardb=\"true\" maxtime=\"50000ms\" barge=\"true\">\n <audio uri=\"file:///appl/wav/simpleplay.wav\"/>\n <playexit>\n   <exit namelist=\"play.end play.amt\"/>\n </playexit>\n</play>\n</dialogstart>\n</msml>"
-      },
-      "type": 3
-    },
-    "event-name": "sip.media.playAnnouncement",
-    "eventname": "callEarlyAnswered",
-    "session": session["fsm-id"],
-    "eventtime": 1739784834579
-  };
+    //default values
+    let mrf_used = {
+        "announcement":"default",
+        "collect": {
+            "cleardb":"true",
+            "edt":"1s",
+            "fdt":"3s",
+            "idt":"2s",
+            "iterate":"1"
+        },
+        "play": {
+            "barge":"true",
+            "maxtime":"11s",
+            "interval":"1",
+            "cleardb":"true",
+            "offset":"0",
+            "audioiterate":"1",
+            "audiouri":"file:///appl/wav/tictac.wav"            
+        },
+        "pattern": {
+            "digits": "[1-2]",
+            "format": "regex"
+        }            
+    };
 
-  "Content": "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<msml  version=\"1.1\">\n<dialogstart name=\"annoPromptCollect\" target=\"conn:\"session["mrf"]["downStreamToTag"] type=\"application/moml+xml\">\n<play interval=\"100ms\" iterate=\"1\" cleardb=\"true\" maxtime=\"50000ms\" barge=\"true\">\n <audio uri=\"file:///appl/wav/simpleplay.wav\"/>\n <playexit>\n   <exit namelist=\"play.end play.amt\"/>\n </playexit>\n</play>\n</dialogstart>\n</msml>"
-*/
+    //default XML header
     let content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<msml  version=\"1.1\">\n<dialogstart name=\"dialognamedefault\" target=\"conn:";
+    //add the dynamic to that from MRF received to tag
     content = content + session["mrf"]["downStreamToTag"];
     content = content + "\" type=\"application/moml+xml\">\n";
+
+    let collect=false;
+    //if this is only a playannouncement
+    if ( session["mrf"]["collect"] != null) {
+        //collect is present. this is a prompt and collect
+        //overwrite default values
+        mrf_used["collect"] = session["mrf"]["collect"];
+        mrf_used["pattern"] = session["mrf"]["pattern"];
+        mrf_used["play"] = session["mrf"]["play"];
+        collect=true;
+    } else {
+        //this is only a play announcement
+        mrf_used["play"] = session["mrf"]["play"];
+        collect=false;
+    }
+
+    if (collect==true) {
+        content = content + "<collect  cleardb=\"" + session["mrf"]["collect"]["cleardb"] + "\" edt=\"" + session["mrf"]["collect"]["edt"]  + "\" fdt=\"" + session["mrf"]["collect"]["fdt"]  + "\" idt=\"" + session["mrf"]["collect"]["idt"]  + "\"  iterate=\"" + session["mrf"]["collect"]["iterate"]  + "\">\n";
+        content = content + "<play barge=\"" + session["mrf"]["play"]["barge"] + "\"> maxtime=\"" + session["mrf"]["play"]["maxtime"] + "\" <audio uri=\"" + session["mrf"]["play"]["audiouri"] + "\"/> </play>\n";
+        content = content + "<pattern digits=\"" + session["mrf"]["pattern"]["digits"] + "\">  <send target=\"source\" event=\"done\" namelist=\"dtmf.digits dtmf.end\"/> </pattern>\n";
+        content = content + "<noinput> <send target=\"source\" event=\"done\" namelist=\"dtmf.end\"/> </noinput>\n";
+        content = content + "<nomatch> <send target=\"source\" event=\"done\" namelist=\"dtmf.end\"/> </nomatch>\n";
+        content = content + "</collect>\n";        
+    } else {
+        content = content + "<play barge=\"" + session["mrf"]["play"]["barge"] + "\" maxtime=\"" + session["mrf"]["play"]["maxtime"] + "\" interval=\"" + session["mrf"]["play"]["interval"] + "\" cleardb=\"" + session["mrf"]["play"]["cleardb"] + "\" offset=\"" + session["mrf"]["play"]["offset"] + "\">\n";
+        content = content + "<audio uri=\"" + session["mrf"]["play"]["audiouri"] + "\"" + "\" iterate=\"" + session["mrf"]["play"]["iterate"] + "\"/>\n";
+        content = content + "<playexit>\n<exit namelist=\"play.end play.amt\"/>\n</playexit>\n";
+        content = content + "</play>";
+    }
+    content = content + "</dialogstart>\n</msml>\n";
+
+    let outevent = {
+        "callid": session["fsm-id"],
+        "event-type": "sip",
+        "queue": "TASV4_1",
+        "id": 2,
+        "action": {
+        "legaction": "performMediaOperation",
+        "performMediaOperation": {
+            "ContentType": "application/msml+xml",
+            "Content": content
+        },
+        "type": 3
+        },
+        "event-name": "sip.media.playAnnouncement",
+        "eventname": "callEarlyAnswered",
+        "session": session["fsm-id"]
+    };
+        /*
+            let outevent = {
+            "callid": session["fsm-id"],
+            "event-type": "sip",
+            "queue": "TASV4_1",
+            "id": 2,
+            "timestamp": 1739784834563,
+            "action": {
+            "legaction": "performMediaOperation",
+            "performMediaOperation": {
+                "ContentType": "application/msml+xml",
+                "Content": "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<msml  version=\"1.1\">\n<dialogstart name=\"annoPromptCollect\" target=\"conn:5b007c40\" type=\"application/moml+xml\">\n<play interval=\"100ms\" iterate=\"1\" cleardb=\"true\" maxtime=\"50000ms\" barge=\"true\">\n <audio uri=\"file:///appl/wav/simpleplay.wav\"/>\n <playexit>\n   <exit namelist=\"play.end play.amt\"/>\n </playexit>\n</play>\n</dialogstart>\n</msml>"
+            },
+            "type": 3
+            },
+            "event-name": "sip.media.playAnnouncement",
+            "eventname": "callEarlyAnswered",
+            "session": session["fsm-id"],
+            "eventtime": 1739784834579
+        };
+
+        "Content": "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<msml  version=\"1.1\">\n<dialogstart name=\"annoPromptCollect\" target=\"conn:\"session["mrf"]["downStreamToTag"] type=\"application/moml+xml\">\n<play interval=\"100ms\" iterate=\"1\" cleardb=\"true\" maxtime=\"50000ms\" barge=\"true\">\n <audio uri=\"file:///appl/wav/simpleplay.wav\"/>\n <playexit>\n   <exit namelist=\"play.end play.amt\"/>\n </playexit>\n</play>\n</dialogstart>\n</msml>"
+        */
+        //let content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<msml  version=\"1.1\">\n<dialogstart name=\"dialognamedefault\" target=\"conn:";
+        //content = content + session["mrf"]["downStreamToTag"];
+        //content = content + "\" type=\"application/moml+xml\">\n";
         //content = content + "<play interval=\"100ms\" iterate=\"1\" cleardb=\"true\" maxtime=\"50000ms\" barge=\"true\">\n <audio uri=\"file:///appl/wav/simpleplay.wav\"/>\n <playexit>\n   <exit namelist=\"play.end play.amt\"/>\n </playexit>\n</play>\n</dialogstart>\n</msml>";
         //OCMP response - groups not supported     <?xml version="1.0" encoding="UTF-8" standalone="yes"?><msml version="1.1">    <result response="402">        <description>Groups not supported</description>    </result></msml>
         //content = content + "<group topology=\"parallel\">\n";
@@ -137,51 +216,27 @@ function SendINFOPromptandCollect(session : any, event : any, localParams: any )
         //content = content + "<detect><send target=\"play.beforebargeplay\" event=\"terminate\"/></detect>\n<noinput><send target=\"source\" event=\"dialognamedefault\" namelist=\"dtmf.digits dtmf.end\"/></noinput>\n<nomatch><send target=\"source\" event=\"dialognamedefault\" namelist=\"dtmf.digits dtmf.end\"/></nomatch>\n</collect>\n";
         //content = content + "<detect><send target=\"source\" event=\"terminate\"/></detect>\n<noinput><send target=\"source\" event=\"dialognamedefault\" namelist=\"dtmf.digits dtmf.end\"/></noinput>\n<nomatch><send target=\"source\" event=\"dialognamedefault\" namelist=\"dtmf.digits dtmf.end\"/></nomatch>\n</collect>\n";
         //content = content + "</group>\n";
-    /* supported OCMP4.8 collect parameters
-    id: an optional identifier that may be referenced elsewhere for sending events to this primitive.
-    cleardb: a boolean indication of whether the buffer for digit collection should be cleared of any collected digits when the element is instantiated.  If set to false, any digits currently in the buffer MUST be immediately compared against the pattern elements.
-    fdt: defines the first-digit timer value.  The first-digit timer is started when DTMF detection is initially invoked.  If no DTMF digits are detected during this initial interval, the <noinput> element MUST be invoked.  Optional, default is 0 s (wait forever for the first digit).
-    idt: defines the inter-digit timer to be used when digits are being collected.  When specified, the timer is started when the first digit is detected and restarted on each subsequent digit. Timer expiration is applied to all patterns.  After that, if any patterns remain active and a nomatch element is specified, the nomatch is executed and DTMF input MUST terminate.  The idt attribute should only be used when digit collection is being performed.  Optional, default is 4 s.
-    edt: defines the extra-digit timer value.  Specifies the length of time the media server MUST wait after a match to detect a termination key, if one is specified by the <pattern> element. Optional, default is 4 s.
-    iterate: specifies the number of times the <pattern>, <noinput>,  and <nomatch> elements may be executed unless those elements specify differently.  The value "forever" MAY be used to indicate that these may be executed any number of times.  Default is once '1'.
-    */
-    /* supported OCMP4.8 play parameters
-    id: an optional identifier that may be referenced elsewhere for sending events to the play primitive.
-    interval: specifies the delay between stopping one iteration and beginning another.  The attribute has no effect if iterate is not also specified.  Default is no interval. 
-    iterate: specifies the number of times the media specified by the child media elements should be played.  Each iteration is a  complete play of each of the child media elements in document order.  Defaults to once '1'.
-    maxtime: defines the maximum allowed time for the <play> to complete.
-        in garge=true the announcement is not stopped at maxtime
-    barge: defines whether or not audio announcements may be  interrupted by DTMF detection during play-out.  The DTMF digit barging the announcement is stored in the digit buffer.  Valid values for barge are "true" or "false", and the attribute is mandatory.  When barge is applied to a conference target, DTMF digit detected from any conference participant MUST terminate the announcement.
-    cleardb: defines whether or not the digit buffer is cleared, prior  to starting the announcement.  Valid values for cleardb are "true" or "false", and the attribute is mandatory. 
-    offset: defines an offset, measured in units of time, where the <play> is to begin media generation.  Offset is only valid when all child media elements are <audio>.
-    */
 
-    content = content + "<collect  cleardb=\"" + session["mrf"]["collect"]["cleardb"] + "\" edt=\"" + session["mrf"]["collect"]["edt"]  + "\" fdt=\"" + session["mrf"]["collect"]["fdt"]  + "\" idt=\"" + session["mrf"]["collect"]["idt"]  + "\"  iterate=\"" + session["mrf"]["collect"]["iterate"]  + "\">\n";
-    content = content + "<play barge=\"" + session["mrf"]["play"]["barge"] + "\"> maxtime=\"" + session["mrf"]["play"]["maxtime"] + "\" <audio uri=\"" + session["mrf"]["play"]["audiouri"] + "\"/> </play>\n";
-    content = content + "<pattern digits=\"" + session["mrf"]["pattern"]["digits"] + "\">  <send target=\"source\" event=\"done\" namelist=\"dtmf.digits dtmf.end\"/> </pattern>\n";
-    content = content + "<noinput>\n         <send target=\"source\" event=\"done\"\n               namelist=\"dtmf.end\"/>\n      </noinput>\n";
-    content = content + "<nomatch>\n         <send target=\"source\" event=\"done\"\n               namelist=\"dtmf.end\"/>\n      </nomatch>\n";
-    content = content + "</collect>\n";
-    content = content + "</dialogstart>\n</msml>\n";
+        /* supported OCMP4.8 collect parameters
+        id: an optional identifier that may be referenced elsewhere for sending events to this primitive.
+        cleardb: a boolean indication of whether the buffer for digit collection should be cleared of any collected digits when the element is instantiated.  If set to false, any digits currently in the buffer MUST be immediately compared against the pattern elements.
+        fdt: defines the first-digit timer value.  The first-digit timer is started when DTMF detection is initially invoked.  If no DTMF digits are detected during this initial interval, the <noinput> element MUST be invoked.  Optional, default is 0 s (wait forever for the first digit).
+        idt: defines the inter-digit timer to be used when digits are being collected.  When specified, the timer is started when the first digit is detected and restarted on each subsequent digit. Timer expiration is applied to all patterns.  After that, if any patterns remain active and a nomatch element is specified, the nomatch is executed and DTMF input MUST terminate.  The idt attribute should only be used when digit collection is being performed.  Optional, default is 4 s.
+        edt: defines the extra-digit timer value.  Specifies the length of time the media server MUST wait after a match to detect a termination key, if one is specified by the <pattern> element. Optional, default is 4 s.
+        iterate: specifies the number of times the <pattern>, <noinput>,  and <nomatch> elements may be executed unless those elements specify differently.  The value "forever" MAY be used to indicate that these may be executed any number of times.  Default is once '1'.
+        */
+        /* supported OCMP4.8 play parameters
+        id: an optional identifier that may be referenced elsewhere for sending events to the play primitive.
+        interval: specifies the delay between stopping one iteration and beginning another.  The attribute has no effect if iterate is not also specified.  Default is no interval. 
+        iterate: specifies the number of times the media specified by the child media elements should be played.  Each iteration is a  complete play of each of the child media elements in document order.  Defaults to once '1'.
+        maxtime: defines the maximum allowed time for the <play> to complete.
+            in garge=true the announcement is not stopped at maxtime
+        barge: defines whether or not audio announcements may be  interrupted by DTMF detection during play-out.  The DTMF digit barging the announcement is stored in the digit buffer.  Valid values for barge are "true" or "false", and the attribute is mandatory.  When barge is applied to a conference target, DTMF digit detected from any conference participant MUST terminate the announcement.
+        cleardb: defines whether or not the digit buffer is cleared, prior  to starting the announcement.  Valid values for cleardb are "true" or "false", and the attribute is mandatory. 
+        offset: defines an offset, measured in units of time, where the <play> is to begin media generation.  Offset is only valid when all child media elements are <audio>.
+        */
 
-    let outevent = {
-    "callid": session["fsm-id"],
-    "event-type": "sip",
-    "queue": "TASV4_1",
-    "id": 2,
-    "action": {
-      "legaction": "performMediaOperation",
-      "performMediaOperation": {
-        "ContentType": "application/msml+xml",
-        "Content": content
-      },
-      "type": 3
-    },
-    "event-name": "sip.media.playAnnouncement",
-    "eventname": "callEarlyAnswered",
-    "session": session["fsm-id"]
-  };
-  return outevent;
+    return outevent;
 }
 
 
